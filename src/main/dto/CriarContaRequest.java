0fc3cce8025a40e2983f8136b95f1744487b2c2a
joaovirone <jovmamikl@gmail.com>
2025-12-11 19:55:16 -0300

    feat: Criada a pasta fto para deixar o projeto mais robusto e com mais segurança

---
+++ src/main/dto/CriarContaRequest.java
@@ -0,0 +1,16 @@
+package dto;
+
+public class CriarContaRequest {
+
+    private String nome;
+    private String cpf;
+
+}
+
+public class CriaContaResponse {
+    
+    private String nome;
+    private String cpf;
+    private String numeroDaConta;
+    private Double saldo;
+}
