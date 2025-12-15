969c115b076267c7238abd0cacab8d3bb73bb7ba
joao <jovmamikl@gmail.com>
2025-12-15 10:47:38 -0300

    feat: criado no model Transacao, ou seja o modelo das transacoes, ele que vai guardar o histórico

---
+++ src/main/java/org/bank/fintech/model/Transacao.java
@@ -0,0 +1,58 @@
+package org.bank.fintech.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valor;
+    private LocalDateTime dataHora;
+
+    @Enumerated(EnumType.STRING)
+    private TipoTransacao tipo;
+    
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+    public Transacao() {}
+
+    public Transacao(Double valor, TipoTransacao tipo, Conta conta){
+        this.valor = valor;
+        this.tipo = tipo;
+        this.conta = conta;
+        this.dataHora = LocalDateTime.now();
+    }
+
+    public Long getId(){
+        return id;
+    }
+    
+    public Double getValor(){
+        return valor;
+    }
+    
+    public LocalDateTime getDataHora(){
+        return dataHora;
+    }
+    
+    public TipoTransacao getTipo(){
+        return tipo;
+    }
+    
+    public Conta getConta(){
+        return conta;
+    }
+}
