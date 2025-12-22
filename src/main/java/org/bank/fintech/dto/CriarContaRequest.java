7b12521922efa089c681e52a978308ed49c10513
joaovirone <jovmamikl@gmail.com>
2025-12-22 20:23:17 -0300

    refactor: aplicado o lombok para otimizar o código

--- src/main/java/org/bank/fintech/dto/CriarContaRequest.java
+++ src/main/java/org/bank/fintech/dto/CriarContaRequest.java
@@ -1,11 +1,31 @@
 package org.bank.fintech.dto;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
 public class CriarContaRequest {
     
-    @NotBlank String titular;
-    @Valid String cpf;
-    Double valorDepositoInicial;
+    @NotBlank (message = "O nome do titular é obrigatório.")
+   private String titular;
+
+    @NotBlank (message = "Número do CPF é obrigatório.")
+    private String cpf;
+
+    @NotNull (message = "A data de nascimento é obrigatória.")
+    private LocalDateTime dataDeNascimento;
+
+    private Double valorInicial = 0.0;
+    
 }
